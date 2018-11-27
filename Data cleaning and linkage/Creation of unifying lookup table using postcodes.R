08cfe21d01cd90875b40e4d4c79e42626643eefc
Meng Le <mzhang@hotmail.co.uk>
2018-11-27 15:04:48 +0000

    The start of my recreation of the data

---
+++ Data cleaning and linkage/Creation of unifying lookup table using postcodes.R
@@ -0,0 +1,78 @@
+########################################################
+##  Census output area to lsoa, la and ttwa 2001      ##
+##  For 2001 we cannot find which census oa maps to   ##
+##  which lsoa. We can create this table ourselves    ##
+##  Start: 7/3/2017                                   ##
+########################################################
+
+##  Pre: Load in all the RCI functions we need
+source.file <- 'RCI functions.R' #path to source
+source(source.file)
+
+##  First: we load in the map and variables datasets-----
+pc.lkp <- google.drive.spatial %>%
+  paste('/geoportal lkps for UK/ONS_Postcode_Directory_Latest_Centroids.csv' , sep = '') %>% 
+  fread(header = T, select = c('pcds', 'lsoa01', 'oa01', 
+                               'oa11', 'lsoa11',
+                               'ttwa'))
+ttwa11.nms <- google.drive.spatial %>%
+  paste('/geoportal lkps for UK/Travel_to_Work_Areas_December_2011_Names_and_Codes_in_the_United_Kingdom.csv', sep = '') %>%
+  read.csv
+
+##  merge the two datasets
+pc.lkp <- pc.lkp %>% merge(ttwa11.nms, by.x = 'ttwa', by.y = 'ï..TTWA11CD')
+
+
+
+##  Second: Using the group_by and summarise feature we can create lookups ----
+oa01tolsoa01.lkp <- pc.lkp %>%
+  group_by(lsoa01, oa01) %>%
+  summarise(count = length(oa01)) %>% 
+  group_by(oa01) %>% ## group by oa to create the weights
+  mutate(weight = (count / sum(count)) %>% round(2)) 
+##  Remember weights tell us how much of the oa belongs in a lsoa so it is 
+##  count / total counts of oa
+##  Confusion will screw us over
+oa01tolsoa01.lkp$weight %>% table #1 = perfect
+
+oa01tolsoa01.lkp %>% 
+  dplyr::select(-count) %>% 
+  write.csv('Working analysis files/oa01 to lsoa01 lkp.csv', row.names = F)
+
+##  oa11 to lsoa11 lkp
+oa11tolsoa01.lkp <- pc.lkp %>%
+  group_by(lsoa01, oa11) %>%
+  summarise(count = length(oa11)) %>% 
+  group_by(oa11) %>%
+  mutate(weight = (count / sum(count)) %>% round(2))
+
+oa11tolsoa01.lkp %>% 
+  dplyr::select(-count) %>% 
+  write.csv('Working analysis files/oa11 to lsoa01 lkp.csv', row.names = F)
+
+##  lsoa11 to lsoa01
+lsoa11tolsoa01.lkp <- pc.lkp %>%
+  group_by(lsoa01, lsoa11) %>%
+  summarise(count = length(lsoa11)) %>% 
+  group_by(lsoa11) %>%
+  mutate(weight = (count / sum(count)) %>% round(2))
+
+lsoa11tolsoa01.lkp %>% 
+  dplyr::select(-count) %>% 
+  write.csv('Working analysis files/lsoa11 to lsoa01 lkp.csv', row.names = F)
+
+##  lsoa01 to ttwa11
+lsoa01tottwa11.lkp <- pc.lkp %>%
+  group_by(ttwa, lsoa01) %>%
+  summarise(count = length(lsoa11)) %>% 
+  group_by(lsoa01) %>%
+  mutate(weight = (count / sum(count)) %>% round(2))
+
+lsoa01tottwa11.lkp$weight %>% table ## its almost perfectly all in to be fair
+
+lsoa01tottwa11.lkp %>% 
+  dplyr::select(-count) %>% 
+  write.csv('Working analysis files/lsoa01 to ttwa11 lkp.csv', row.names = F)
+
+##End
+rm(list = ls()) #finish by removing all
---
+++ Data cleaning and linkage/Joining all the data for England and Wales.R
@@ -0,0 +1,91 @@
+##  Creating the unified field of data to be used for the analysis.
+##  So this entails getting a file of LSOA01 and then working with it
+
+##  Pre: Load in all the RCI functions we need
+source.file <- 'RCI functions.R' #path to source
+source(source.file)
+
+##  First: we load in the variables datasets / lookup files-----
+base.df <- google.drive.spatial %>% 
+  paste('/geoportal lkps for UK/Lower_Layer_Super_Output_Areas_December_2001_Names_and_Codes_in_Northern_Ireland.csv', sep = '') %>%
+  read.csv %>%
+  rename(LSOA01CD = ï..LSOAN05CD)
+
+oa01tolsoa01.lkp <-
+  read.csv('Working analysis files/oa01 to lsoa01 lkp.csv')
+
+oa11tolsoa01.lkp <-
+  read.csv('Working analysis files/oa11 to lsoa01 lkp.csv')
+
+lsoa11tolsoa01.lkp <-
+  read.csv('Working analysis files/lsoa11 to lsoa01 lkp.csv')
+
+lsoa01tottwa11.lkp <-
+  read.csv('Working analysis files/lsoa01 to ttwa11 lkp.csv')
+
+##  Second: We need to get the data from various sources into lsoa01cd -----
+##  Population ----
+##  2001 data
+pop.2001 <- google.drive.spatial %>% 
+  paste('/LSOA 2001/LSOA pop census 2001.csv', sep ='') %>%
+  read.csv
+pop.2001 %>% names
+pop.2001 <- pop.2001 %>% 
+  mutate(adult.pop01 = Age..All.usual.residents..measures..Value - sum(c(5:10,17:20)),
+         LSOA01CD = geography.code) %>% # minus sum of some rows
+  dplyr::select(LSOA01CD, adult.pop01)
+
+
+##  2011 data
+
+
+##  JSA----
+##  2001
+jsa2001 <- google.drive.spatial %>% 
+          paste('/DWP data/jsa200105.csv', sep = '') %>%
+  read.csv
+
+jsa2001 <- jsa2001 %>% 
+  mutate(LSOA01CD = Lower.Layer.SOA...Data.Zone.Code,
+         jsa01 = Total) %>%
+  dplyr::select(LSOA01CD, jsa01)
+
+##  2011
+jsa2011 <- google.drive.spatial %>% 
+  paste('/DWP data/jsa201105.csv', sep = '') %>%
+  read.csv
+
+jsa2011 <- jsa2011 %>% 
+  mutate(LSOA01CD = Lower.Layer.SOA...Data.Zone.Code,
+         jsa11 = Total) %>%
+  dplyr::select(LSOA01CD, jsa11)
+
+##  Workplace pop ----
+oa.wplace01 <- google.drive.spatial %>%
+  paste('/OA 2001/workplace oa 2001.csv', sep = '') %>%
+  read.csv
+
+oa.wplace01 <- oa.wplace01 %>%
+  mutate(oa01 = substr(Area, 8 , 17),
+         wplace01 = X2001 %>% as.numeric) %>%
+  dplyr::select(oa01, wplace01)
+
+wplace2001 <- oa.wplace01 %>% merge(oa01tolsoa01.lkp)
+wplace2001 <- wplace2001 %>%
+  group_by(lsoa01) %>%
+  summarise(wplace01 = sum(wplace01))
+
+
+##  Adding coordinates for the population weighted centroids. ----
+lsoa01cent <- st_read(dsn = google.drive.spatial %>% 
+                        paste('/LSOA 2001', sep = ''), 
+                      layer='Lower_Layer_Super_Output_Areas_December_2001_Population_Weighted_Centroids')
+
+lsoa01cent <- data.frame(LSOA01CD = lsoa01cent$lsoa01cd,
+                         lsoa01cent %>% st_coordinates)
+lsoa01cent %>% str ## in easting northings
+
+##  3) Merging all the data in section 2
+
+##  >Now we save tge ew.2001 data set for the future
+save(file='../Data/Analysis data/England and Wales 0111 temp.Rdata',ew.2001)
