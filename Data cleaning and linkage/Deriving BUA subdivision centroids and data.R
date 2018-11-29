3c5ad05698f0d85890a3d0665198a160838e0d28
Meng Le <mzhang@hotmail.co.uk>
2018-11-29 13:52:28 +0000

    Rejigged our approach to bua centroids from using the lookup table (which has issues) to using the GIS files to identify which lsoa fall where (using points)

--- Data cleaning and linkage/Deriving BUA subdivision centroids and data.R
+++ Data cleaning and linkage/Deriving BUA subdivision centroids and data.R
@@ -4,20 +4,39 @@
 ##  Pre: Load in all the RCI functions we need
 source.file <- 'RCI functions.R' #path to source
 source(source.file)
+ukgrid = "+init=epsg:27700" ## always remember the CRS
 
-##  1) Load in the data for BUA subdivision lookups
-lsoa11tobua11.lkp <- google.drive.spatial %>% 
-  paste('/geoportal lkps for UK/LSOA2011 to BUA and BUASD 2011.csv', sep ='') %>%
-  read.csv
+### 1) Load in the data for BUA and LSOA pop weighted centroids
+bua.sf <- st_read(
+  dsn = google.drive.spatial %>%
+    paste('/BUA 2011', sep = ''),
+  layer = 'Builtup_Areas_December_2011_Boundaries_V2') %>%
+  st_transform(crs = st_crs(ukgrid))
 
-##  2) Load in lsoa11 data with centroids and merge with pop data
+bua.sf <- bua.sf %>% 
+  filter(has_sd == 'N' & urban_bua == 'Yes') %>%
+  mutate(name = bua11nm, 
+         type = 'bua') %>%
+  dplyr::select(name, type, geometry) #keep those without subdivision and is urban
+###
+buasd.sf <- st_read(
+  dsn = google.drive.spatial %>%
+    paste('/BUA subdivision 2011', sep = ''),
+  layer = 'Builtup_Area_Sub_Divisions_December_2011_Boundaries') %>%
+  st_transform(crs = st_crs(ukgrid))
+
+buasd.sf <- buasd.sf %>% 
+  mutate(name = buasd11nm, 
+         type = 'buasd') %>%
+  dplyr::select(name, type, geometry) #keep those without subdivision and is urban
+
+
+#################
+##  2) Load in lsoa11 data with centroids and merge with pop data -----------
 lsoa11.sf <- google.drive.spatial %>% 
   paste('/LSOA 2011', sep ='') %>%
   st_read('Lower_Layer_Super_Output_Areas_December_2011_Population_Weighted_Centroids') %>%
   st_transform(crs = st_crs(ukgrid))
-
-lsoa11.cent <- data.frame(lsoa11cd = lsoa11.sf$lsoa11cd,
-                          lsoa11.sf %>% st_coordinates)
 
 pop.2011 <- google.drive.spatial %>% 
   paste('/LSOA 2011/LSOA pop census 2011.csv', sep ='') %>%
@@ -27,23 +46,34 @@ pop.2011 <- pop.2011 %>%
   mutate(total = Age..All.usual.residents..measures..Value,
          lsoa11cd = geography.code)
 
-pop.2011 <- pop.2011 %>% merge(lsoa11.cent)
+lsoa11.sf <- lsoa11.sf %>% merge(pop.2011)
+lsoa11.sf <- lsoa11.sf %>% cbind(lsoa11.sf %>% st_coordinates)
 
-
-##  3) Now to just easily calulcate the pop weighted centroids
-lsoa11tobua11.lkp %>% head
-pop.weighted %>% head
-pop.weighted <- lsoa11tobua11.lkp %>% merge(pop.2011, by.x = 'LSOA11CD', by.y = 'lsoa11cd')
-pop.weighted <- pop.weighted %>%
-  group_by(BUASD11NM) %>%
+##  3) Do a spatial join of the lsoa file to bua and buasd then caluclate stats -------
+##  First for the bua
+lsoa11tobua.sf <- lsoa11.sf %>% st_join(bua.sf)
+st_geometry(lsoa11tobua.sf) <- NULL
+bua.weighted <-   lsoa11tobua.sf%>% 
+  group_by(name) %>%
   summarise(pweight_x = X %>% weighted.mean(total),
             pweight_y = Y %>% weighted.mean(total),
-            pop11 = sum(total))
+            pop11 = sum(total),
+            number_lsoa = total %>% length,
+            type = 'bua')
 
-##  Big error in the data!
-(lsoa11tobua11.lkp$BUASD11NM == '') %>% table # some have no buasd11nm
-lsoa11tobua11.lkp %>% filter(BUASD11NM == '') #ggr some have subdivisions but others do not!
-lsoa11tobua11.lkp %>% filter(BUASD11NM == '' & BUA11NM == '')
-### STOPPED HERE
-pop.weighted %>%
-  write.csv('Working analysis files/buasd11 pop weighted centroids and other data.csv', row.names = F)
+lsoa11tobuasd.sf <- lsoa11.sf %>% st_join(buasd.sf)
+st_geometry(lsoa11tobuasd.sf) <- NULL
+buasd.weighted <-   lsoa11tobuasd.sf%>% 
+  group_by(name) %>%
+  summarise(pweight_x = X %>% weighted.mean(total),
+            pweight_y = Y %>% weighted.mean(total),
+            pop11 = sum(total),
+            number_lsoa = total %>% length,
+            type = 'buasd')
+
+
+all.weighted <- rbind(bua.weighted, buasd.weighted)
+
+##  Now to save
+all.weighted %>%
+  write.csv('Working analysis files/bua and buasd11 pop weighted centroids and other data.csv', row.names = F)
--- Generating derived variables/Querying osm for centres.R
+++ Generating derived variables/Querying osm for centres.R
@@ -2,12 +2,12 @@
 ##  Implementation requires the osmdata package to query osm
 #options(repos = c(CRAN = "https://cran.revolutionanalytics.com")) # code to use different repository than default
 
-pkgs <- c('osmdata', 'tmap', 'sf', 'tidyverse')
+pkgs <- c('osmdata', 'tmap', 'sf', 'tidyverse', 'RANN')
 lapply(pkgs, require, character.only = T)
 ukgrid = "+init=epsg:27700" ## always remember the CRS
 
 
-# Section 1: Querying osm and saving the data -----------------------------
+# Section 1: Querying osm and saving the data (can skip if query done) -----------------------------
 eng_bb <- getbb('england, uk') # Extent of England
 wales_bb <- getbb('wales') # Extent of Wales
 eng_bb; wales_bb ## actually england has the max and min
@@ -46,76 +46,45 @@ landmarks_sf %>% saveRDS('Working analysis files/Large files to ignore/Saved Cen
 
 # Section 2: Filtering and finding locality centre points -----------------
 landmarks_sf <- readRDS('Working analysis files/Large files to ignore/Saved Centre landmarks from osmdata.RDS')
+landmarks.coords <- data.frame(type = landmarks_sf$type, 
+                               st_coordinates(landmarks_sf)) ##  we only need points
 
-##  Let's go and find the localities which is inclusive of non divied up settlements 
-##
-##  Load in the geo files
-##  localities
-loc.sf <- st_read(dsn = google.drive %>% 
-                    paste('/Spatial lookup files/UK/Scottish settlements', sep = ''), 
-                  layer = 'Localities2012') %>%
+##  We have to basically get all the bua and bua subdivisions going
+all.weighted <-
+  read.csv('Working analysis files/bua and buasd11 pop weighted centroids and other data.csv')
+pop_centroids.sf <- all.weighted %>% 
+  st_as_sf(coords = c('pweight_x', 'pweight_y'),
+           crs = st_crs(ukgrid))
+
+
+# 3) Loading in the bua and buasd bondary files ---------------------------
+
+bua.sf <- st_read(
+  dsn = google.drive.spatial %>%
+    paste('/BUA 2011', sep = ''),
+  layer = 'Builtup_Areas_December_2011_Boundaries_V2') %>%
   st_transform(crs = st_crs(ukgrid))
 
-loc.pop <- google.drive %>% 
-  paste('/Spatial lookup files/UK/Scottish settlements/loc2012 pop.csv', sep = '') %>%
-  read.csv
+bua.sf <- bua.sf %>% 
+  filter(has_sd == 'N' & urban_bua == 'Yes') %>%
+  mutate(name = bua11nm, 
+         type = 'bua') %>%
+  dplyr::select(name, type, geometry) #keep those without subdivision and is urban
+###
+buasd.sf <- st_read(
+  dsn = google.drive.spatial %>%
+    paste('/BUA subdivision 2011', sep = ''),
+  layer = 'Builtup_Area_Sub_Divisions_December_2011_Boundaries') %>%
+  st_transform(crs = st_crs(ukgrid))
 
-loc.pop <- loc.pop %>% 
-  mutate(L12CODE = Locality.Code, locality.pop = All.Ages) %>% 
-  select(L12CODE, locality.pop, Locality.Name) 
+buasd.sf <- buasd.sf %>% 
+  mutate(name = buasd11nm, 
+         type = 'buasd') %>%
+  dplyr::select(name, type, geometry) #keep those without subdivision and is urban
 
-loc.sf <- loc.sf %>% merge(loc.pop)
-
-##  We can use st_join to literally do a left join that adds the variables of
-##  the polygons to any point that intersects (i.e. falls within) that polygon
-landmarks_sf <- landmarks_sf %>% 
-  st_join(loc.sf) %>%
-  dplyr::select(geometry, L12CODE, L12NAME, name, osm_id, locality.pop, type)
-
-##  save it as a csv
-landmarks_csv <- data.frame(landmarks_sf %>% st_coordinates,
-                            L12CODE = landmarks_sf$L12CODE,
-                            landmarknm = landmarks_sf$name,
-                            type = landmarks_sf$type)
-landmarks_csv %>% write.csv('Saved files for linkage/centre landmarks for localities coords.csv')
+all_bua.sf <- bua.sf %>% rbind(buasd.sf)
 
 
-##  Viewing statistics
-landmarks_sf
-loc.pop %>% 
-  filter(!(L12CODE %in% landmarks_sf$L12CODE)) %>% 
-  filter(locality.pop > 10000) %>%
-  arrange(locality.pop)
+# 4) Running the for loop to get the centres ------------------------------
 
-loc.pop %>% 
-  filter(!(L12CODE %in% loc.sf$L12CODE)) %>% 
-  arrange(locality.pop)
-##  okay missing is due to non entries
-##  Not every locality has every local feature! however... the smallest locality
-##  is 43k -- we can just omit???
 
-##  There are only 86 or so localities over 10k anyway
-loc.pop %>% 
-  filter(locality.pop > 10000) %>%
-  arrange(-locality.pop) %>%
-  mutate(cumulative = cumsum(locality.pop) / sum(locality.pop))
-
-##  okay so if we do it this way then the cut off is over 20k and leads to 38 places
-##  It is perfectly possile to cope with 38 places!
-loc.pop %>% 
-  filter(!(L12CODE %in% landmarks_sf$L12CODE)) %>% 
-  filter(locality.pop > 30000) %>%
-  arrange(locality.pop)
-
-##  Filtering
-sel.loc <- loc.pop %>% filter(locality.pop > 30000) %>% select(L12CODE)
-
-selected <- landmarks_sf %>% 
-  filter(L12CODE %in% sel.loc$L12CODE) %>%
-  group_by(L12NAME) %>%
-  summarise(n = n()) 
-## 23 
-loc.pop %>% 
-  filter(!(L12CODE %in% landmarks_sf2$L12CODE)) %>% 
-  filter(locality.pop > 30000) %>%
-  arrange(locality.pop)
